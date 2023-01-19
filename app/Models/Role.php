d4d8a880cc43e373bd702e75e720d7651c638703
altwaireb <altwaireb@gmail.com>
2023-01-19 09:02:05 +0300

    ADD static function default

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
@@ -11,7 +13,7 @@ class Role extends Model
     use HasFactory;
     use SoftDeletes;
 
-    protected $fillable = ['name' , 'key' , 'color', 'default'];
+    protected $fillable = ['name', 'key', 'color', 'default'];
 
     protected $dates = ['deleted_at'];
 
@@ -19,12 +21,12 @@ class Role extends Model
         'default' => 'boolean',
     ];
 
-    public function users()
+    public function users(): HasMany
     {
         return $this->hasMany(User::class);
     }
 
-    public function permissions()
+    public function permissions(): BelongsToMany
     {
         return $this->belongsToMany(Permission::class);
     }
@@ -35,17 +37,31 @@ class Role extends Model
      * @param $key
      * @return bool
      */
-    public function hasPermission($key)
+    public function hasPermission($key): bool
     {
         return $this->permissions()->where('key', $key)->exists();
     }
 
-    public function scopeSearch($query, $term){
-        $query->where(function ($query) use ($term){
-            $query->where('name','like', "%$term%")
-                ->orWhere('key','like', "%$term%")
-                ->orWhere('color','like', "%$term%");
+    public function scopeSearch($query, $term)
+    {
+        $query->where(function ($query) use ($term) {
+            $query->where('name', 'like', "%$term%")
+                ->orWhere('key', 'like', "%$term%")
+                ->orWhere('color', 'like', "%$term%");
         });
+    }
+
+    /**
+     * Get ID Role if column default
+     * value is True
+     */
+    public static function default()
+    {
+        $data = self::where('default',1)->first();
+        if (!empty($data)){
+            return $data->id;
+        }
+        return null;
     }
 
 }
